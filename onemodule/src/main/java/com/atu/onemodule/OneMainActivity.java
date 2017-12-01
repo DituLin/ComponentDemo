bba4c77ee8e0de776fce05230520c7ef4b88361f
林地土 <linditu@nicigo.com>
2017-12-01 10:58:53 +0800

    Initial commit

---
+++ onemodule/src/main/java/com/atu/onemodule/OneMainActivity.java
@@ -0,0 +1,13 @@
+package com.atu.onemodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OneMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one_activity_main);
+    }
+}
---
+++ secondmodule/src/main/java/com/atu/secondmodule/SecondMainActivity.java
@@ -0,0 +1,13 @@
+package com.atu.secondmodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity_main);
+    }
+}
